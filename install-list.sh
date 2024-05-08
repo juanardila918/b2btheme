5ccd604614e8f378b566579ac820b7122e480262
Juan Ardila <juan.felipe@vtex.com>
2024-05-08 17:56:55 -0500

    first commit

---
+++ install-list.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -e
+
+vtex install vtex.reviews-and-ratings@3.x
+vtex install vtex.admin@4.x
+vtex install vtex.admin-customers@2.x
+vtex install vtex.admin-home@3.x
+vtex install vtex.b2b-checkout-settings@1.x
+vtex install vtex.b2b-orders-history@0.x
+vtex install vtex.b2b-organizations@1.x
+vtex install vtex.b2b-quotes@1.x
+vtex install vtex.checkout-ui-custom@0.x
+vtex install vtex.my-account@1.x
+vtex install vtex.quickorder@3.x
+vtex install vtex.storefront-permissions-ui@1.x
+vtex install vtex.wish-list@1.x
+vtex install vtex.location-availability@0.x
+vtex install vtex.shopper-location@0.x
+vtex install vtex.wordpress-integration@2.x
+vtex install vtex.search-resolver@1.x
+vtex install vtex.admin-graphql-ide@3.x
+vtex install vtex.admin-promotions@1.x
+vtex install vtex.admin-search@1.x
+vtex install vtex.admin-webpage-performance@0.x
+vtex install vtex.customer-credit@4.x
+vtex install vtex.my-credits@1.x
+vtex install vtex.pagespeed-graphql@0.x
+vtex install vtex.social-selling@0.x
+vtex install vtex.storefront-permissions@1.x
+vtex install vtex.telemarketing@2.x
+vtex install ssesandbox04.threekit-app@0.x
+vtex install ssesandbox04.seller-page@0.x
+vtex install ssesandbox04.b2b-suite-bra@1.x
